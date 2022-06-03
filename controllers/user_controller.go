6f334467f2d1d840bf001b44e03139f4ecd31a4a
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-03 03:18:17 -0700

    controller

---
+++ controllers/user_controller.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"context"
+	"go-stac-api/configs"
+	"go-stac-api/models"
+	"go-stac-api/responses"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "users")
+var validate = validator.New()
+
+func CreateUser(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var user models.User
+	defer cancel()
+
+	//validate the request body
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	//use the validator library to validate required fields
+	if validationErr := validate.Struct(&user); validationErr != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
+	}
+
+	newUser := models.User{
+		Id:       primitive.NewObjectID(),
+		Name:     user.Name,
+		Location: user.Location,
+		Title:    user.Title,
+	}
+
+	result, err := userCollection.InsertOne(ctx, newUser)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	return c.Status(http.StatusCreated).JSON(responses.UserResponse{Status: http.StatusCreated, Message: "success", Data: &fiber.Map{"data": result}})
+}
