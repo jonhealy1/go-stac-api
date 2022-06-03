cb4ae996f2929182193d8e7846ebc93a82e8b3d5
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-03 03:32:59 -0700

    add routes

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -8,4 +8,8 @@ import (
 
 func UserRoute(app *fiber.App) {
 	app.Post("/user", controllers.CreateUser)
+	app.Get("/user/:userId", controllers.GetAUser)
+	app.Put("/user/:userId", controllers.EditAUser)
+	app.Delete("/user/:userId", controllers.DeleteAUser)
+	app.Get("/users", controllers.GetAllUsers)
 }
