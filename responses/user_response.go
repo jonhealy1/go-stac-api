e28025453de0f0f942ceb72f8ead4ef9522b94c2
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-03 04:54:06 -0700

    delete old functions

--- responses/user_response.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-import "github.com/gofiber/fiber/v2"
-
-type UserResponse struct {
-	Status  int        `json:"status"`
-	Message string     `json:"message"`
-	Data    *fiber.Map `json:"data"`
-}
