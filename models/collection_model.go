c435c7be7edfc6d47d6ce2e94ed7ccc89eabbf4f
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-06 03:52:28 -0700

    change model

--- models/collection_model.go
+++ models/collection_model.go
@@ -3,8 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Collection struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
-	Name     string             `json:"name,omitempty" validate:"required"`
-	Location string             `json:"location,omitempty" validate:"required"`
-	Title    string             `json:"title,omitempty" validate:"required"`
+	Mongo_id     primitive.ObjectID `json:"mongo_id,omitempty"`
+	Id           string             `json:"id,omitempty" validate:"required"`
+	Stac_version string             `json:"stac_version,omitempty" validate:"required"`
+	Description  string             `json:"description,omitempty" validate:"required"`
 }
