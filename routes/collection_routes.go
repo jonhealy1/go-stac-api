f7339cc90e5519bffeaad7d919283ede2d6b6ca3
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-06 23:51:28 -0700

    update method names

--- routes/collection_routes.go
+++ routes/collection_routes.go
@@ -8,8 +8,8 @@ import (
 
 func UserRoute(app *fiber.App) {
 	app.Post("/collections", controllers.CreateCollection)
-	app.Get("/collections/:collectionId", controllers.GetACollection)
-	app.Put("/collections/:collectionId", controllers.EditACollection)
-	app.Delete("/collections/:collectionId", controllers.DeleteACollection)
-	app.Get("/collections", controllers.GetAllCollections)
+	app.Get("/collections/:collectionId", controllers.GetCollection)
+	app.Put("/collections/:collectionId", controllers.EditCollection)
+	app.Delete("/collections/:collectionId", controllers.DeleteCollection)
+	app.Get("/collections", controllers.GetCollections)
 }
