dbc990373254be5ed46bc4d39aa17febb08a3b43
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-17 00:39:14 -0700

    get item method

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -46,8 +47,22 @@ func CreateItem(c *fiber.Ctx) error {
 
 	result, err := userItem.InsertOne(ctx, newItem)
 	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(responses.CollectionResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+		return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
 	}
 
 	return c.Status(http.StatusCreated).JSON(responses.ItemResponse{Status: http.StatusCreated, Message: "success", Data: &fiber.Map{"data": result}})
+}
+
+func GetItem(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	itemId := c.Params("itemId")
+	var item models.Collection
+	defer cancel()
+
+	err := userItem.FindOne(ctx, bson.M{"id": itemId}).Decode(&item)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	return c.Status(http.StatusOK).JSON(responses.ItemResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": item}})
 }
