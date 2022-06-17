17846d485d8eb2565b0757671dcf7a87ac8ce0a0
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-17 00:14:52 -0700

    update item route

--- routes/item_routes.go
+++ routes/item_routes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ItemRoute(app *fiber.App) {
-	app.Post("/collections/items", controllers.CreateItem)
+	app.Post("/collections/:collectionId/items", controllers.CreateItem)
 	// app.Get("/items/:collectionId", controllers.GetItem)
 	// app.Put("/items/:collectionId", controllers.EditItem)
 	// app.Delete("/items/:collectionId", controllers.DeleteItem)
