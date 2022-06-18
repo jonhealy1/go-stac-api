8e76a2625cb2051261fcc77c88a0bd4d57c16424
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-18 08:17:03 -0700

    tests working

--- tests/routes_test.go
+++ tests/routes_test.go
@@ -1,11 +1,18 @@
+// ./go/testing.go
+
 package routes
 
 import (
+	"context"
+	"go-stac-api/configs"
+	"go-stac-api/models"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert" // add Testify package
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func TestCollectionsRoute(t *testing.T) {
@@ -52,4 +59,27 @@ func TestCollectionsRoute(t *testing.T) {
 		// Verify, if the status code is as expected
 		assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
 	}
+}
+
+func TestGetCollection(t *testing.T) {
+	var stacCollection *mongo.Collection = configs.GetCollection(configs.DB, "collections")
+	app := fiber.New()
+	req := httptest.NewRequest("GET", "/collections/sentinel-s2-l2a-cogs", nil)
+	resp, _ := app.Test(req, 1)
+	assert.Equal(t, 404, resp.StatusCode, "status ok")
+
+	newCollection := models.Collection{
+		Id:          "sentinel-s2-l2a-cogs",
+		StacVersion: "1.0.0",
+		Description: "Sentinel-2a and Sentinel-2b imagery, processed to Level 2A (Surface Reflectance) and converted to Cloud-Optimized GeoTIFFs",
+		Title:       "Sentinel 2 L2A COGs",
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stacCollection.InsertOne(ctx, newCollection)
+
+	req = httptest.NewRequest("GET", "/collections/sentinel-s2-l2a-cogs", nil)
+	resp, _ = app.Test(req)
+	//body, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, 200, resp.StatusCode, "status ok")
 }
