f7db85710d1f693ac03410f3872c5e20f3ba2600
jonhealy1 <jonathan.d.healy@gmail.com>
2022-06-19 01:42:30 -0700

    delete item route

--- routes/item_routes.go
+++ routes/item_routes.go
@@ -10,7 +10,6 @@ func ItemRoute(app *fiber.App) {
 	app.Post("/collections/:collectionId/items", controllers.CreateItem)
 	app.Get("/collections/:collectionId/items/:itemId", controllers.GetItem)
 	app.Get("/collections/:collectionId/items", controllers.GetItemCollection)
-	// app.Put("/items/:collectionId", controllers.EditItem)
-	// app.Delete("/items/:collectionId", controllers.DeleteItem)
-	// app.Get("/items", controllers.GetItems)
+	// app.Put("/collections/:collectionId/items/:itemId", controllers.EditItem)
+	app.Delete("/collections/:collectionId/items/:itemId", controllers.DeleteItem)
 }
