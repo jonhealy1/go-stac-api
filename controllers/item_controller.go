460f27a077ea998b92347ba9c00b9677237d6f1c
jonhealy1 <jonathan.d.healy@gmail.com>
2022-08-30 04:43:11 -0700

    search items, collection

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -233,8 +233,7 @@ func EditItem(c *fiber.Ctx) error {
 func PostSearch(c *fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	var search models.Search
-	// var items []models.Item
-	var item models.Item
+	var items []models.Item
 	defer cancel()
 
 	//validate the request body
@@ -247,11 +246,45 @@ func PostSearch(c *fiber.Ctx) error {
 		return c.Status(http.StatusBadRequest).JSON(responses.ItemResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
 	}
 
-	err := stacItem.FindOne(ctx, bson.M{"id": search.Ids[0]}).Decode(&item)
-
-	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	count := 0
+	if search.Collection != "" {
+		results, err := stacItem.Find(ctx, bson.M{"id": bson.M{"$in": search.Ids}, "collection": search.Collection})
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+		}
+		defer results.Close(ctx)
+		for results.Next(ctx) {
+			var singleItem models.Item
+			if err = results.Decode(&singleItem); err != nil {
+				return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+			}
+			count = count + 1
+			items = append(items, singleItem)
+		}
+	} else {
+		results, err := stacItem.Find(ctx, bson.M{"id": bson.M{"$in": search.Ids}})
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+		}
+		defer results.Close(ctx)
+		for results.Next(ctx) {
+			var singleItem models.Item
+			if err = results.Decode(&singleItem); err != nil {
+				return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+			}
+			count = count + 1
+			items = append(items, singleItem)
+		}
 	}
 
-	return c.Status(http.StatusOK).JSON(responses.ItemResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": item}})
+	itemCollection := models.ItemCollection{
+		Type:     "FeatureCollection",
+		Features: items,
+	}
+
+	return c.Status(http.StatusOK).JSON(
+		responses.ItemResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"count": count, "data": itemCollection}},
+	)
+
+	//return c.Status(http.StatusOK).JSON(responses.ItemResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": results}})
 }
