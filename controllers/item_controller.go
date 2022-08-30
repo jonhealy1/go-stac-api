27acae06cb4f66152cec6104163744696610a8a6
jonhealy1 <jonathan.d.healy@gmail.com>
2022-08-30 00:56:05 -0700

    create post search function

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -220,3 +220,38 @@ func EditItem(c *fiber.Ctx) error {
 
 	return c.Status(http.StatusOK).JSON(responses.ItemResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": updatedItem}})
 }
+
+// PostSearch godoc
+// @Summary POST Search request
+// @Description Search for STAC items via the Search endpoint
+// @Tags Search
+// @ID post-search
+// @Accept  json
+// @Produce  json
+// @Param search body models.Search true "Search body json"
+// @Router /search
+func PostSearch(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var search models.Search
+	// var items []models.Item
+	var item models.Item
+	defer cancel()
+
+	//validate the request body
+	if err := c.BodyParser(&search); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.ItemResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	//use the validator library to validate required fields
+	if validationErr := validate_item.Struct(&search); validationErr != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.ItemResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
+	}
+
+	err := stacItem.FindOne(ctx, bson.M{"id": search.Ids[0]}).Decode(&item)
+
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.ItemResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	return c.Status(http.StatusOK).JSON(responses.ItemResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": item}})
+}
