760c0cb4d5ca5da4cf84648b4be44e702872cfaf
jonhealy1 <jonathan.d.healy@gmail.com>
2022-08-31 06:24:15 -0700

    add geojson models

--- models/search_model.go
+++ models/search_model.go
@@ -1,14 +1,43 @@
 package models
 
+import "encoding/json"
+
 type Search struct {
-	Ids         []string  `json:"ids,omitempty"`
-	Collections []string  `json:"collections,omitempty"`
-	Limit       int       `json:"limit,omitempty"`
-	Bbox        []float64 `json:"bbox,omitempty"`
-	Geometry    GeoJson   `json:"geometry,omitempty"`
+	Ids         []string                  `json:"ids,omitempty"`
+	Collections []string                  `json:"collections,omitempty"`
+	Limit       int                       `json:"limit,omitempty"`
+	Bbox        []float64                 `json:"bbox,omitempty"`
+	Geometry    GeoJSONGeometryCollection `json:"geometry,omitempty"`
 }
 
-type GeoJson struct {
-	Type        string    `json:"type"`
-	Coordinates []float64 `json:"coordinates"`
+type GeoJSONGeometryCollection struct {
+	Type       string            `json:"type"` // will always be "GeometryCollection"
+	Geometries []json.RawMessage `json:"geometries"`
+}
+
+type GeoJSONGenericGeometry struct {
+	Type        string          `json:"type"`
+	Coordinates json.RawMessage `json:"coordinates"`
+}
+
+type GeoJSONPoint struct {
+	Type        string     `json:"type"`
+	Coordinates [2]float64 `json:"coordinates"`
+}
+
+// line or multipoint
+type GeoJSONLine struct {
+	Type        string       `json:"type"`
+	Coordinates [][2]float64 `json:"coordinates"`
+}
+
+// polygon or multiline
+type GeoJSONPolygon struct {
+	Type        string         `json:"type"`
+	Coordinates [][][2]float64 `json:"coordinates"`
+}
+
+type GeoJSONMultiPolygon struct {
+	Type        string           `json:"type"`
+	Coordinates [][][][2]float64 `json:"coordinates"`
 }
