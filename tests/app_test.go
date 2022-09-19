cb8a019d33a4cb4ddae13bdaf90930c2036dadf8
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-19 11:18:54 +0300

    create collection test

--- tests/app_test.go
+++ tests/app_test.go
@@ -1,11 +1,15 @@
 package tests
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"go-stac-api/configs"
 	"go-stac-api/models"
+	"go-stac-api/responses"
 	"go-stac-api/routes"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"testing"
@@ -37,11 +41,36 @@ func Setup() *fiber.App {
 	return app
 }
 
-func TestCollectionsRoute(t *testing.T) {
-	// Define a structure for specifying input and output
-	// data of a single test case. This structure is then used
-	// to create a so called test map, which contains all test
-	// cases, that should be run for testing this function
+func TestCreateCollection(t *testing.T) {
+	var expected_collection models.Collection
+	jsonFile, err := os.Open("setup_data/collection.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &expected_collection)
+	responseBody := bytes.NewBuffer(byteValue)
+
+	resp, err := http.Post("http://localhost:6001/collections", "application/json", responseBody)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+
+	assert.Equalf(t, 201, resp.StatusCode, "create collection")
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var collection_response responses.CollectionResponse
+	json.Unmarshal(body, &collection_response)
+
+	assert.Equal(t, "success", collection_response.Message, "create collection")
+}
+func TestGetCollection(t *testing.T) {
 	LoadCollection()
 	LoadItems()
 
@@ -63,15 +92,15 @@ func TestCollectionsRoute(t *testing.T) {
 		expectedCode  int
 		expectedBody  models.Collection
 	}{
-		// {
-		// 	description:   "root catalog route",
-		// 	route:         "/",
-		// 	expectedError: false,
-		// 	expectedCode:  200,
-		// 	expectedBody:  "{\"stac_version\":\"1.0.0\",\"id\":\"test-catalog\",\"title\":\"go-stac-api\",\"description\":\"test catalog for go-stac-api, please edit\",\"links\":[{\"rel\":\"self\",\"href\":\"/\",\"type\":\"application/json\",\"title\":\"root catalog\"},{\"rel\":\"children\",\"href\":\"/collections\",\"type\":\"application/json\",\"title\":\"stac child collections\"}]}",
-		// },
 		{
-			description:   "all collections route",
+			description:   "GET collection route",
+			route:         "/collections/sentinel-s2-l2a-cogs-test",
+			expectedError: false,
+			expectedCode:  200,
+			expectedBody:  expected_collection,
+		},
+		{
+			description:   "GET collection route",
 			route:         "/collections/sentinel-s2-l2a-cogs-test",
 			expectedError: false,
 			expectedCode:  200,
@@ -97,7 +126,7 @@ func TestCollectionsRoute(t *testing.T) {
 		res, err := app.Test(req, -1)
 
 		// // verify that no error occured, that is not expected
-		// assert.Equalf(t, test.expectedError, err != nil, test.description)
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
 
 		// As expected errors lead to broken responses, the next
 		// test case needs to be processed
@@ -121,5 +150,67 @@ func TestCollectionsRoute(t *testing.T) {
 
 		// Verify, that the reponse body equals the expected body
 		assert.Equalf(t, test.expectedBody, stac_collection, test.description)
+	}
+}
+
+func TestGetAllCollections(t *testing.T) {
+	tests := []struct {
+		description string
+		route       string
+
+		expectedError bool
+		expectedCode  int
+	}{
+		{
+			description:   "GET collections route",
+			route:         "/collections",
+			expectedError: false,
+			expectedCode:  200,
+		},
+	}
+
+	// Setup the app as it is done in the main function
+	app := Setup()
+
+	// Iterate through test single test cases
+	for _, test := range tests {
+		// Create a new http request with the route
+		// from the test case
+		req, _ := http.NewRequest(
+			"GET",
+			test.route,
+			nil,
+		)
+
+		// Perform the request plain with the app.
+		// The -1 disables request latency.
+		res, err := app.Test(req, -1)
+
+		// // verify that no error occured, that is not expected
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		// As expected errors lead to broken responses, the next
+		// test case needs to be processed
+		if test.expectedError {
+			continue
+		}
+
+		// Verify if the status code is as expected
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		// Read the response body
+		body, err := ioutil.ReadAll(res.Body)
+
+		var stac_collection []models.Collection
+
+		json.Unmarshal(body, &stac_collection)
+
+		// length := len(stac_collection)
+
+		// assert.Equal(t, length, 1, test.description)
+
+		// Reading the response body should work everytime, such that
+		// the err variable should be nil
+		assert.Nilf(t, err, test.description)
 	}
 }
