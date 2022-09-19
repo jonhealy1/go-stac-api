efa5e06615a1d73409b7902f11ddb414be51bb24
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-19 15:29:46 +0300

    get collection

--- tests/item_routes_test.go
+++ tests/item_routes_test.go
@@ -44,3 +44,73 @@ func TestCreateItem(t *testing.T) {
 
 	assert.Equalf(t, "success", collection_response.Message, "create collection")
 }
+
+func TestGetItem(t *testing.T) {
+	var expected_item models.Item
+	jsonFile, _ := os.Open("setup_data/S2B_1CCV_20181004_0_L2A-test.json")
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &expected_item)
+
+	tests := []struct {
+		description   string
+		route         string
+		expectedError bool
+		expectedCode  int
+		expectedBody  models.Item
+	}{
+		{
+			description:   "GET collection route",
+			route:         "/collections/sentinel-s2-l2a-cogs-test/items/S2B_1CCV_20181004_0_L2A-test",
+			expectedError: false,
+			expectedCode:  200,
+			expectedBody:  expected_item,
+		},
+	}
+
+	// Setup the app as it is done in the main function
+	app := Setup()
+
+	// Iterate through test single test cases
+	for _, test := range tests {
+		// Create a new http request with the route
+		// from the test case
+		req, _ := http.NewRequest(
+			"GET",
+			test.route,
+			nil,
+		)
+
+		// Perform the request plain with the app.
+		// The -1 disables request latency.
+		res, err := app.Test(req, -1)
+
+		// // verify that no error occured, that is not expected
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		// As expected errors lead to broken responses, the next
+		// test case needs to be processed
+		if test.expectedError {
+			continue
+		}
+
+		// Verify if the status code is as expected
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		// Read the response body
+		body, err := ioutil.ReadAll(res.Body)
+		assert.Nilf(t, err, "get item")
+
+		var stac_item models.Item
+
+		json.Unmarshal(body, &stac_item)
+
+		// Reading the response body should work everytime, such that
+		// the err variable should be nil
+		assert.Nilf(t, err, test.description)
+
+		// Verify, that the reponse body equals the expected body
+		assert.Equalf(t, test.expectedBody, stac_item, test.description)
+	}
+}
