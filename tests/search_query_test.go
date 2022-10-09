31eb9df19835a61a81a16e43de1c2c5bd77ab585
jonhealy1 <jonathan.d.healy@gmail.com>
2022-10-09 15:20:10 +0300

    test query eq

---
+++ tests/search_query_test.go
@@ -0,0 +1,47 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"go-stac-api/models"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchQueryEq(t *testing.T) {
+
+	// Setup the app as it is done in the main function
+	app := Setup()
+
+	body := []byte(`{"query": {"eo:cloud_cover": {"eq": 96.91}}}`)
+
+	req, _ := http.NewRequest(
+		"POST",
+		"/search",
+		bytes.NewBuffer(body),
+	)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Perform the request plain with the app.
+	// The -1 disables request latency.
+	res, err := app.Test(req, -1)
+
+	// // verify that no error occured, that is not expected
+	assert.Equalf(t, false, err != nil, "sort asc test")
+
+	// Verify if the status code is as expected
+	assert.Equalf(t, 200, res.StatusCode, "sort asc test")
+
+	// Read the response body
+	resp_body, err := ioutil.ReadAll(res.Body)
+	assert.Nilf(t, err, "query eq")
+
+	var bodyResponse models.ItemResponse
+
+	json.Unmarshal(resp_body, &bodyResponse)
+
+	assert.Equal(t, bodyResponse.Data.Features[0].Properties["eo:cloud_cover"], 96.91)
+}
