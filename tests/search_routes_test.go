c95d8425cbdc45bad59a67ad86abbc1e7551b0e4
jonhealy1 <jonathan.d.healy@gmail.com>
2022-10-09 11:01:38 +0300

    fields include test

--- tests/search_routes_test.go
+++ tests/search_routes_test.go
@@ -89,3 +89,67 @@ func TestSearchFieldsExclude(t *testing.T) {
 		assert.Equalf(t, test.expectedBody.Properties, bodyResponse.Data.Features[0].Properties, test.description)
 	}
 }
+
+func TestSearchFieldsInclude(t *testing.T) {
+	var expected_item models.Item
+
+	jsonFile, _ := os.Open("setup_data/S2B_1CCV_20181004_0_L2A-test.json")
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &expected_item)
+
+	tests := []struct {
+		description   string
+		route         string
+		expectedError bool
+		expectedCode  int
+		expectedBody  models.Item
+	}{
+		{
+			description:   "POST search fields test route",
+			route:         "/search",
+			expectedError: false,
+			expectedCode:  200,
+			expectedBody:  expected_item,
+		},
+	}
+
+	app := Setup()
+
+	body := []byte(`{"fields": {"include":["properties.created"]}}`)
+
+	for _, test := range tests {
+		req, _ := http.NewRequest(
+			"POST",
+			test.route,
+			bytes.NewBuffer(body),
+		)
+		req.Header.Add("Content-Type", "application/json")
+
+		res, err := app.Test(req, -1)
+
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		if test.expectedError {
+			continue
+		}
+
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		body, err := ioutil.ReadAll(res.Body)
+		assert.Nilf(t, err, "get item")
+
+		var bodyResponse models.ItemResponse
+
+		json.Unmarshal(body, &bodyResponse)
+
+		assert.Nilf(t, err, test.description)
+
+		assert.Nil(t, bodyResponse.Data.Features[0].Assets)
+		assert.Nil(t, bodyResponse.Data.Features[0].Links)
+		assert.Nil(t, bodyResponse.Data.Features[0].Properties["eo:cloud_cover"])
+
+		assert.Equalf(t, test.expectedBody.Properties["created"], bodyResponse.Data.Features[0].Properties["created"], test.description)
+	}
+}
