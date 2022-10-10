d6e3a4fe34da60602993a40a94907a939d676dc3
jonhealy1 <jonathan.d.healy@gmail.com>
2022-10-10 12:54:50 +0300

    test search geometry collection

--- tests/search_geometry_test.go
+++ tests/search_geometry_test.go
@@ -170,3 +170,43 @@ func TestSearchPolygon(t *testing.T) {
 	assert.LessOrEqual(t, bodyResponse.Data.Features[0].Bbox[3], -65.760298)
 	assert.LessOrEqual(t, -78.076921, bodyResponse.Data.Features[0].Bbox[3])
 }
+
+func TestSearchGeometryCollection(t *testing.T) {
+	app := Setup()
+
+	body := []byte(`{"geometry": {
+		"type": "GeometryCollection", 
+		"geometries": [
+			{
+                "type": "Point",
+                "coordinates": [178.01, -72.31]
+            }
+		]
+	}}`)
+
+	req, _ := http.NewRequest(
+		"POST",
+		"/search",
+		bytes.NewBuffer(body),
+	)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := app.Test(req, -1)
+
+	assert.Equalf(t, false, err != nil, "bbox test")
+
+	assert.Equalf(t, 200, res.StatusCode, "bbox test")
+
+	resp_body, err := ioutil.ReadAll(res.Body)
+	assert.Nilf(t, err, "query dateime")
+
+	var bodyResponse models.ItemResponse
+
+	json.Unmarshal(resp_body, &bodyResponse)
+
+	assert.LessOrEqual(t, bodyResponse.Data.Features[0].Bbox[0], 178.01642)
+	assert.LessOrEqual(t, 178.01642, bodyResponse.Data.Features[0].Bbox[2])
+
+	assert.LessOrEqual(t, bodyResponse.Data.Features[0].Bbox[1], -72.31064)
+	assert.LessOrEqual(t, -72.31064, bodyResponse.Data.Features[0].Bbox[3])
+}
