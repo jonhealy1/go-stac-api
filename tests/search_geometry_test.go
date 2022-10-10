6d3262d5200e789b486eebab8acb1441d7443b67
jonhealy1 <jonathan.d.healy@gmail.com>
2022-10-10 14:11:14 +0300

    search bbox no results

--- tests/search_geometry_test.go
+++ tests/search_geometry_test.go
@@ -50,6 +50,37 @@ func TestSearchBbox(t *testing.T) {
 	assert.LessOrEqual(t, -78.076921, bodyResponse.Data.Features[0].Bbox[3])
 }
 
+func TestSearchBboxNoResults(t *testing.T) {
+	app := Setup()
+
+	body := []byte(`{
+		"bbox": [147.795950,18.076921,179.557669,15.760298],
+		"collections": ["sentinel-s2-l2a-cogs-test"]
+	}`)
+
+	req, _ := http.NewRequest(
+		"POST",
+		"/search",
+		bytes.NewBuffer(body),
+	)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := app.Test(req, -1)
+
+	assert.Equalf(t, false, err != nil, "bbox test")
+
+	assert.Equalf(t, 200, res.StatusCode, "bbox test")
+
+	resp_body, err := ioutil.ReadAll(res.Body)
+	assert.Nilf(t, err, "query dateime")
+
+	var bodyResponse models.ItemResponse
+
+	json.Unmarshal(resp_body, &bodyResponse)
+
+	assert.Equal(t, bodyResponse.Data.Context.Returned, 0)
+}
+
 func TestSearchPoint(t *testing.T) {
 	app := Setup()
 
